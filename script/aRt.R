587789b954297d66e56daf3396d8923fafe6fd1f
Marco Chiapello <chiapello.m@gmail.com>
2020-04-10 06:59:23 +0200

    Prepare to send email when the job is done

--- script/aRt.R
+++ script/aRt.R
@@ -1,50 +1,17 @@
-library(dplyr)
 library(jasmines)
 library(patchwork)
 library(tidyverse)
+library(blastula)
+library(here)
 
-layout <- c(
-    area(t = 1, l = 1, b = 2, r = 2),
-    area(t = 3, l = 1, b = 4, r = 2),
-    area(t = 1, l = 3, b = 2, r = 4),
-    area(t = 3, l = 3, b = 4, r = 4),
-    area(t = 1, l = 5, b = 2, r = 6),
-    area(t = 3, l = 5, b = 4, r = 6),
-    area(t = 1, l = 7, b = 2, r = 8),
-    area(t = 3, l = 7, b = 4, r = 8),
-    area(t = 1, l = 9, b = 2, r = 10),
-    area(t = 3, l = 9, b = 4, r = 10),
-    #
-    area(t = 5, l = 1, b = 6, r = 2),
-    area(t = 7, l = 1, b = 8, r = 2),
-    area(t = 5, l = 3, b = 6, r = 4),
-    area(t = 7, l = 3, b = 8, r = 4),
-    area(t = 5, l = 5, b = 6, r = 6),
-    area(t = 7, l = 5, b = 8, r = 6),
-    area(t = 5, l = 7, b = 6, r = 8),
-    area(t = 7, l = 7, b = 8, r = 8),
-    area(t = 5, l = 9, b = 6, r = 10),
-    area(t = 7, l = 9, b = 8, r = 10),
-    #
-    area(t = 9, l = 1, b = 10, r = 2),
-    area(t = 11, l = 1, b = 12, r = 2),
-    area(t = 9, l = 3, b = 10, r = 4),
-    area(t = 11, l = 3, b = 12, r = 4),
-    area(t = 9, l = 5, b = 10, r = 6),
-    area(t = 11, l = 5, b = 12, r = 6),
-    area(t = 9, l = 7, b = 10, r = 8),
-    area(t = 11, l = 7, b = 12, r = 8),
-    area(t = 9, l = 9, b = 10, r = 10),
-    area(t = 11, l = 9, b = 12, r = 10),
-    #
-    area(t = 13, l = 1, b = 14, r = 2),
-    area(t = 13, l = 3, b = 14, r = 4)
-)
-plot(layout)
+################################################################################
+# Inizialize the project
+# renv::init()
+setwd(here())
 
-p <- vector(mode = "list", length = 32)
-col <- palette_named()
-for (i in 1:32){
+p <- vector(mode = "list", length = 2)
+col <- palette_named()[24:25]
+for (i in 1:2){
 p[[i]] <- use_seed(1) %>%
     scene_discs(
         rings = 3, points = 1500, size = 5
@@ -67,11 +34,16 @@ p[[i]] <- use_seed(1) %>%
     ) +
     coord_fixed(ratio = 1)
 }
-p[[1]] + p[[2]] + p[[3]] + p[[4]]  + p[[5]] + p[[6]] + p[[7]] + p[[8]] + p[[9]] + p[[10]] +
-    plot_layout(design = layout)
+
+wrap_plots(p)
 
 
-wrap_plots(p, ncol = 5)
+email <- render_email("script/email.Rmd")
 
-
-
+email %>%
+    smtp_send(
+              from = "chiapello.m@gmail.com",
+              to = "chiapello.m@gmail.com",
+              subject = "aRt project",
+              credentials = creds_key(id = "Marco Chiapello")
+    )
